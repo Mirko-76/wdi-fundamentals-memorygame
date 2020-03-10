a322d695dd19e1111b6d13448eda173191eaf8b6
Mirko Mujica <mirkomujica@192-168-1-104.tpgi.com.au>
2020-03-10 11:45:11 +1100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,25 @@
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cards = ['queen', 'queen', 'king', 'king'];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+let cardTwo = cards[2]
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped: "+cardsInPlay[0]);
+console.log("User flipped: "+cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	console.log("two cards flipped");
+}
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");;
+}
+else {
+	alert("Sorry, try again.");
+}
